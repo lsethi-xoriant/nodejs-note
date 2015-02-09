81faa60e449447285faecb48278e14dad1e1bbea
Tommy Tang <1530234656@qq.com>
2015-02-09 09:33:29 +0800

    update

---
+++ proexpress/ch14/stylus/app.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var path = require("path");
+var favicon = require("static-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+
+var routes = require("./routes/index");
+var users = require("./routes/users");
+
+var app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(require("stylus").middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+app.set('port', process.env.PORT||3000);
+var server= app.listen(app.get('port'), function () {
+    console.log('server listening on :', app.get('port'));
+});
