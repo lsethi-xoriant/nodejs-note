ee09d8805eda5e3438ded39ae92b8df5e7949a33
Tommy Tang <1530234656@qq.com>
2015-03-04 18:20:16 +0800

    update

---
+++ proexpress/ch18/derby-app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by tommytang on 3/2/15.
+ */
+var path = require("path");
+var derby = require("derby");
+var app = derby.createApp('derby-app', __filename);
+
+module.exports = app;
---
+++ proexpress/ch18/server.js
@@ -0,0 +1,4 @@
+/**
+ * Created by tommytang on 3/2/15.
+ */
+require("derby-starter").run(__dirname + '/derby-app.js');
